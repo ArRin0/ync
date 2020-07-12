7e41ca9fee3367a9c7e0ad5ecaa0878bebffb057
StephensunCurry <44474798+StephensunCurry@users.noreply.github.com>
2020-07-12 11:16:59 +0800

    会话界面“结束会话”弹窗

---
+++ src/components/EndSession.vue
@@ -0,0 +1,36 @@
+<template>
+	<div class="box">
+		<el-button type="text" @click="dialogFormVisible = true"><span class="dialog-btn"><i class="iconfont icon-tuichu"></i>结束会话</span></el-button>
+		<el-dialog title="结束会话确认" :visible.sync="dialogFormVisible" class="abow_dialog">
+        <el-form :model="form">
+			<el-form-item label="确定要结束当前会话吗？" :label-width="formLabelWidth">
+			</el-form-item>
+
+		</el-form>
+		  <div slot="footer" class="dialog-footer">
+		    <el-button @click="dialogFormVisible = false">取 消</el-button>
+		    <el-button type="primary" @click="dialogFormVisible = false">确 定</el-button>
+			</div>
+		</el-dialog>
+	</div>
+</template>
+
+<script>
+	 export default {
+		name:"CreateWorkOrder",
+		data() {
+	      return {
+	        dialogFormVisible: false,
+	        form: {
+			
+			  description: '',
+
+	        },
+	        formLabelWidth: '240px'
+	      };
+	    }
+	  };
+</script>
+
+<style>
+</style>
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -58,7 +58,7 @@ const routes = [{
 	},
 	{
 		path: '/test',
-		component: () => import('@/components/Block.vue'),
+		component: () => import('@/components/EndSession.vue'),
 	},
 ]
 
