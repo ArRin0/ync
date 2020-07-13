cf32a655f690b73cd80ceeee18fc827fe9f1cb26
StephensunCurry <44474798+StephensunCurry@users.noreply.github.com>
2020-07-13 22:11:41 +0800

    “基础设置”完成

--- src/pages/settings/order/basicsetting.vue
+++ src/pages/settings/order/basicsetting.vue
@@ -1,10 +1,36 @@
 <template>
 	<div class="page">
 		<nav-bar></nav-bar>
-		<g-header name="通知公告"></g-header>
+		<g-header name="基础设置"></g-header>
 		<leftlist></leftlist>
 		<div class="container">
-			<div class="choice"><drop></drop></div>
+			<div class="overview-style">
+			  <div>
+			    <label style="font-size: 14px">通知工单创建人</label>
+			    <el-switch
+			      v-model="value1" style="padding-left: 10px">
+			    </el-switch>
+			    <br/>
+			    <br/>
+			    <label style="color: #C0C4CC;font-size: 14px;">工单被更新时，同步通知工单创建人</label>
+			    <el-divider></el-divider>
+			  </div>
+			  <div>
+			    <label style="font-size: 14px">自动接单状态</label>
+			    <el-switch
+			      v-model="value2" style="padding-left: 25px">
+			    </el-switch>
+			    <br/>
+			    <br/>
+			    <label style="color: #C0C4CC;font-size: 14px;">关闭时，将不会分配给您工单。</label>
+			    <el-divider></el-divider>
+			  </div>
+			  <div>
+			    <label style="font-size: 14px;padding-right: 10px">工单回复可见范围</label>
+			    <el-radio v-model="radio" label="1">仅客服可见</el-radio>
+			    <el-radio v-model="radio" label="2">所有人可见</el-radio>
+			  </div>
+			</div>
 		</div>
 		
 	</div>
@@ -23,18 +49,32 @@
 			NavBar,
 			GHeader,
 		},
+		data() {
+		  return {
+		      value1: true,
+		      value2: true,
+		      radio: '1',
+		  };
+		},
+		methods: {},
+		created() {},
+		mounted() {}
 	}
 </script>
+<style scoped>
+  .overview-style {
+    padding: 20px;
+    background-color: #fff;
+    border-radius: 5px;
+  }
 
+</style>
 <style>
 	.page {
 		position: relative;
 		width: 1680px;
 	}
-	.main-area{
-		width: 1326px;
-		margin: 10px 74px;
-	}
+
 	.container{
 		width: 1040px;
 		height: 1869px;
