786bc0105ef3ebfdb48167b7dc4d1a262073680f
Colin <1198015529@qq.com>
2020-07-13 19:26:59 +0800

    setting系列

---
+++ src/pages/settings/common/notice.vue
@@ -0,0 +1,48 @@
+<template>
+	<div class="page">
+		<nav-bar></nav-bar>
+		<g-header name="通知公告"></g-header>
+		<leftlist></leftlist>
+		<div class="container">
+			<div class="choice"><drop></drop></div>
+		</div>
+		
+	</div>
+</template>
+
+<script>
+	import leftlist from '@/components/LeftListOfSetting.vue'
+	import NavBar from '@/components/NavBar.vue'
+	import GHeader from '@/components/Header.vue'
+	import drop from '../components/drop.vue'
+	
+	 export default {
+		name:"personal",
+		components: {
+			leftlist,
+			NavBar,
+			GHeader,
+			drop
+		},
+	}
+</script>
+
+<style>
+	.page {
+		position: relative;
+		width: 1680px;
+	}
+	.container{
+		width: 1040px;
+		height: 1869px;
+		background-color: #FFFFFF;
+		border-radius: 4px;
+		position: absolute;
+		top: 74px;
+		left: 324px;
+	}
+	.choice{
+		width: 1040px;
+		height: 70px;
+	}
+</style>
