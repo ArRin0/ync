0ddf46d5ab83f4def1d8f4f45d13c16572678daf
郝庆龙 <1343573831@qq.com>
2020-07-13 20:06:10 +0800

    个人信息连接

--- src/pages/settings/personal.vue
+++ src/pages/settings/personal.vue
@@ -18,23 +18,23 @@
 					</el-form-item>
 					
 					<el-form-item label="邮箱地址" :label-width="formLabelWidth"  :required="true">
-					  <el-input class="input" v-model="form.address" autocomplete="off" placeholder="该邮箱将作为客服人员的登录账号"></el-input>
+					  <el-input class="input" v-model="form.email" autocomplete="off" placeholder="该邮箱将作为客服人员的登录账号"></el-input>
 					</el-form-item>
 					
 					<el-form-item label="客服昵称" :label-width="formLabelWidth"  :required="true">
-					  <el-input class="input" v-model="form.nickname" autocomplete="off" placeholder="与外部客服沟通"></el-input>
+					  <el-input class="input" v-model="form.nickName" autocomplete="off" placeholder="与外部客服沟通"></el-input>
 					</el-form-item>
 					
 					<el-form-item label="真实姓名" :label-width="formLabelWidth" :required="true">
-					  <el-input class="input" v-model="form.name" autocomplete="off" placeholder="请输入"></el-input>
+					  <el-input class="input" v-model="form.realName" autocomplete="off" placeholder="请输入"></el-input>
 					</el-form-item>
 					
 					<el-form-item label="客户工号" :label-width="formLabelWidth">
-					  <el-input class="input" v-model="form.id" autocomplete="off" placeholder="请输入"></el-input>
+					  <el-input class="input" v-model="form.customerServiceId" autocomplete="off" placeholder="请输入"></el-input>
 					</el-form-item>
 					
 					<el-form-item label="客服手机号码" :label-width="formLabelWidth" >
-					  <el-input class="input" v-model="form.number" autocomplete="off" placeholder="请输入"></el-input>
+					  <el-input class="input" v-model="form.phone" autocomplete="off" placeholder="请输入"></el-input>
 					</el-form-item>
 					
 					<el-divider></el-divider>
@@ -42,21 +42,21 @@
 					<div style="width: 56px;height: 19px; margin-left: 27px; margin-bottom: 20px;"><span>密码设置</span></div>
 					
 					<el-form-item label="原密码" :label-width="formLabelWidth">
-					  <el-input class="input" type="password" v-model="form.oldpwd" autocomplete="off" placeholder="请输入原密码"></el-input>
+					  <el-input class="input" type="password" v-model="form.oldPwd" autocomplete="off" placeholder="请输入原密码"></el-input>
 					</el-form-item>
 					
 					<el-form-item label="新密码" :label-width="formLabelWidth" >
-					  <el-input class="input" type="password" v-model="form.newpwd" autocomplete="off" placeholder="请输入新密码"></el-input>
+					  <el-input class="input" type="password" v-model="form.password" autocomplete="off" placeholder="请输入新密码"></el-input>
 					</el-form-item>
 					
 					<el-form-item label="确认新密码" :label-width="formLabelWidth">
-					  <el-input class="input" type="password" v-model="form.confirm" autocomplete="off" placeholder="请再次输入密码"></el-input>
+					  <el-input class="input" type="password" v-model="form.password" autocomplete="off" placeholder="请再次输入密码"></el-input>
 					</el-form-item>
 					
 					<el-divider></el-divider>
 				</el-form>
 				
-				  <div class="save"><el-button  @click="doSubmit;">保存</el-button></div>
+				  <div class="save"><el-button  @click="doSubmit">保存</el-button></div>
 				
 			   </el-container>
 			</div>
@@ -79,13 +79,13 @@
 		data() {
 	      return {
 			  form: {
-			    address:'',
-				nickname:'',
-				name:'',
-				id:'',
-				number:'',
-				oldpwd:'',
-				newpdw:'',
+			    email:'',
+				nickName:'',
+				realName:'',
+				customerServiceId:'',
+				phone:'',
+				oldPwd:'',
+				password:'',
 				confirm:'',
 			  },
 	        formLabelWidth: '120px'
@@ -94,7 +94,20 @@
 		methods:{
 			doSubmit:function(data){
 				console.log(this.form);
-			}
+				this.$axios
+					.post("cs/update", this.form) //请求接口
+					.then(resp => { //当服务器返回结果后处理
+						let {
+							data
+						} = resp;
+						if(data.success){
+						}
+					})
+					.catch(err => { //当请求失败，处理
+						console.log(err)
+					})
+			},
+			
 		}
 			
 	  };
